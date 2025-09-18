b5346b913ff34d249f8d4e18e53957fc2644aac6
Dylden <dng.dyl@gmail.com>
2025-09-18 12:37:04 +0200

    test pull

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,14 +5,40 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ScoreRepository;
+use App\Entity\Score;
+use App\Form\ScoreType;
+
 
 final class GameController extends AbstractController
 {
     #[Route('/game', name: 'game')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, ScoreRepository $scoreRepository): Response
     {
+
+        $score = new Score();
+
+    
+
+        $form = $this->createForm(ScoreType::class, $score);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // Enregistrer le score dans la base de données
+            $entityManager->persist($score);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Score ajouté avec succès !');
+
+        }
+
+        $bestScores = $scoreRepository->findBy([], ['time' => 'ASC'], 4);
+
         return $this->render('game/index.html.twig', [
-            
+            'form' => $form->createView(),
+            'bestScores' => $bestScores,
         ]);
     }
 }
