1b0bb80b57166baf0697e4b0026fb3160b2c0079
Dylden <dng.dyl@gmail.com>
2025-09-19 11:46:33 +0200

    correction envoi de score

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,7 +52,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_user');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Form/ScoreType.php
+++ src/Form/ScoreType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class ScoreType extends AbstractType
 {
@@ -14,7 +15,7 @@ class ScoreType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('time');
+            ->add('time', HiddenType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
